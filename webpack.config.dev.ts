bab822ddc3b32431ed264e18453729618e6bf281
Ben Rogers <ben.rogers@jobsite.co.uk>
2017-04-21 10:13:15 +0100

    Corrected removal of babel/eslint

--- webpack.config.dev.ts
+++ webpack.config.dev.ts
@@ -57,7 +57,6 @@ export default {
   module: {
     rules: [
       {test: /\.tsx?$/, loader: 'awesome-typescript-loader'},
-      {test: /\.jsx?$/, exclude: /node_modules/, loaders: ['babel-loader']},
       {test: /\.eot(\?v=\d+.\d+.\d+)?$/, loader: 'file-loader'},
       {test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'url-loader?limit=10000&mimetype=application/font-woff'},
       {test: /\.[ot]tf(\?v=\d+.\d+.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=application/octet-stream'},
